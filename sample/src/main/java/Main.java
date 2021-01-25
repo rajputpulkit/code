6d70656b84c30f460a135727cf47ab5e7d1b6868
rajputpulkit <77944221+rajputpulkit@users.noreply.github.com>
2021-01-25 09:54:33 +0530

    file A

--- sample/src/main/java/Main.java
+++ sample/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world"); System.out.println("Yeah man");
+        System.out.println("Hello world");
+        System.out.println("Yeah man");
+        A a = new A();
+        String gf = a.play(3);
+        System.out.println(gf);
     }
 }
